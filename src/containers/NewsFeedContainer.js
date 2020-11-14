39aa492ccbdf007d26ad01838fe143e3e18c569f
Jamison Sheppard <jsheppard@neucadia.com>
2020-11-13 21:04:57 -0500

    Added reselect

--- src/containers/NewsFeedContainer.js
+++ src/containers/NewsFeedContainer.js
@@ -2,10 +2,10 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { loadNews } from '../actions/newsActions';
 import NewsFeed from '../components/NewsFeed';
-import { reshapeNewsData } from '../util/dataTransformations';
+import { allNewsSelector } from '../selectors/newsSelectors';
 
 const mapStateToProps = state => ({
-  news: reshapeNewsData(state.news)
+  news: allNewsSelector(state)
 });
 
 const mapDispatchToProps = dispatch => (
