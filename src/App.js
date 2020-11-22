d809468f9ca78a2d4b835cb8659b52d67a6ac4a8
Jamison Sheppard <jsheppard@neucadia.com>
2020-11-22 12:35:11 -0500

    Refactored tabs for react native navigation v5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import HomeScreen from './components/HomeScreen';
 import createStore from './createStore';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import NewsFeedContainer from './containers/NewsFeedContainer';
+import WorldNewsContainer from './containers/WorldNewsContainer';
+import OpinionContainer from './containers/OpinionContainer';
 
 const store = createStore();
 
+const Tab = createBottomTabNavigator();
+
 export default () => (
   <Provider store={store}>
-    <HomeScreen />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="US" component={NewsFeedContainer} />
+        <Tab.Screen name="World" component={WorldNewsContainer} />
+        <Tab.Screen name="Opinion" component={OpinionContainer} />
+      </Tab.Navigator>
+    </NavigationContainer>
   </Provider>
 );
